e3c997ad4a47d0eb58e6f3af12d5438f9dcf827b
Signal2NoiseR <61147059+Signal2NoiseR@users.noreply.github.com>
2020-02-18 21:52:01 +0900

    17-21行追加

--- assessment.js
+++ assessment.js
@@ -14,6 +14,12 @@ function removeAllChildren(element) {
   }
 }
 
+userNameInput.onkeydown = (event) => {
+  if (event.key === 'Enter') {
+    assessmentButton.onclick();
+  }
+};
+
 assessmentButton.onclick = () => {
   const userName = userNameInput.value;
   if (userName.length === 0) { // 名前が空の時は処理を終了する
